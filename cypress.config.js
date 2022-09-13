b64e77978a1a43fe231b48e9eec28c5df4377db7
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-09-13 19:26:11 +0300

    11.9, fix eslint - cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -9,8 +10,8 @@ module.exports = defineConfig({
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "webpack",
+      framework: 'react',
+      bundler: 'webpack',
     },
   },
-});
+})
